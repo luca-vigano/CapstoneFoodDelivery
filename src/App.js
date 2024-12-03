ec2b2fc001b6c6b2b5cd12cf8d9378642636fa8a
luca-vigano <luca.vigano@hotmail.com>
2024-12-03 15:59:15 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import { Home } from "./component/Navbar/Home/Home";
 import RestaurantDetails from "./component/Restaurant/RestaurantDetails";
 import Cart from "./component/Cart/Cart";
 import Profile from "./component/Profile/Profile";
+import CustomerRoute from "./Routers/CustomerRoute";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Home /> */}
       {/* <RestaurantDetails /> */}
       {/* <Cart /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <CustomerRoute />
     </ThemeProvider>
   );
 }

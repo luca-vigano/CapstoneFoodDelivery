2eb16f1d0267cdfae5f2892366bea9c30305883b
luca-vigano <luca.vigano@hotmail.com>
2024-12-16 15:08:29 +0100

    event card fix

--- src/component/AdminComponent/Events/Events.jsx
+++ src/component/AdminComponent/Events/Events.jsx
@@ -1,12 +1,24 @@
-import { Box, Button, Grid2, Modal, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid2,
+  IconButton,
+  Modal,
+  TextField,
+} from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createEventAction } from "../../State/Restaurant/Action";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import {
+  createEventAction,
+  getRestaurantEvents,
+} from "../../State/Restaurant/Action";
+import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
+import EventCard from "../../Profile/EventCard";
+import CreateIcon from "@mui/icons-material/Create";
 
 const style = {
   position: "absolute",
@@ -46,8 +58,8 @@ const Events = () => {
   const token = localStorage.getItem("token");
   const { restaurant } = useSelector((store) => store);
 
+  console.log("Restaurant DATA", restaurant);
   const handleSubmit = (values) => {
-    console.log("event DATA", values);
     dispatch(
       createEventAction({
         data: values,
@@ -58,122 +70,138 @@ const Events = () => {
     setOpen(false);
   };
 
-  return (
-    <div>
-      <div className="p-5">
-        <Button onClick={handleOpen} variant="contained">
-          Create New Event
-        </Button>
+  useEffect(() => {
+    dispatch(
+      getRestaurantEvents({
+        restaurantId: restaurant.usersRestaurant?.id,
+        token,
+      })
+    );
+  }, []);
 
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={handleSubmit}
-            >
-              {({ values, handleChange, setFieldValue, errors, touched }) => (
-                <Form>
-                  <Grid2 className="my-3" container spacing={3}>
-                    <Grid2 size={{ xs: 12 }}>
-                      <Field
-                        name="image"
-                        label="Image URL"
-                        variant="outlined"
-                        fullWidth
-                        as={TextField}
-                        onChange={handleChange}
-                        value={values.image}
-                        error={touched.image && Boolean(errors.image)}
-                        helperText={touched.image && errors.image}
-                      />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12 }}>
-                      <Field
-                        name="location"
-                        label="Location"
-                        variant="outlined"
-                        fullWidth
-                        as={TextField}
-                        onChange={handleChange}
-                        value={values.location}
-                        error={touched.location && Boolean(errors.location)}
-                        helperText={touched.location && errors.location}
-                      />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12 }}>
-                      <Field
-                        name="name"
-                        label="Event Name"
-                        variant="outlined"
-                        fullWidth
-                        as={TextField}
-                        onChange={handleChange}
-                        value={values.name}
-                        error={touched.name && Boolean(errors.name)}
-                        helperText={touched.name && errors.name}
-                      />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12 }}>
-                      <Field
-                        name="description"
-                        label="Event Description"
-                        variant="outlined"
-                        fullWidth
-                        as={TextField}
-                        multiline
-                        rows={4}
-                        onChange={handleChange}
-                        value={values.description}
-                        error={
-                          touched.description && Boolean(errors.description)
-                        }
-                        helperText={touched.description && errors.description}
-                      />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12 }}>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateTimePicker
-                          renderInput={(props) => <TextField {...props} />}
-                          label="Start Date and Time"
-                          value={values.startedAt}
-                          onChange={(newValue) =>
-                            setFieldValue("startedAt", newValue)
-                          }
-                          className="w-full"
-                          sx={{ width: "100%" }}
-                        />
-                      </LocalizationProvider>
-                    </Grid2>
-                    <Grid2 size={{ xs: 12 }}>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateTimePicker
-                          renderInput={(props) => <TextField {...props} />}
-                          label="End Date and Time"
-                          value={values.endsAt}
-                          onChange={(newValue) =>
-                            setFieldValue("endsAt", newValue)
-                          }
-                          className="w-full"
-                          sx={{ width: "100%" }}
-                        />
-                      </LocalizationProvider>
-                    </Grid2>
-                  </Grid2>
-                  <Button className="my-2" variant="contained" type="submit">
-                    Create Event
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-          </Box>
-        </Modal>
+  return (
+    <div className="lg:px-20 px-5 pb-10">
+      <div className="py-5 flex justify-between items-center">
+        <h1 className="text-2xl lg:text-7xl text-center font-bold p-5">
+          Events
+        </h1>
+        <IconButton onClick={handleOpen} aria-label="create event">
+          <CreateIcon />
+        </IconButton>
       </div>
+
+      <Grid2 container spacing={3}>
+        {restaurant.restaurantsEvents?.map((event) => (
+          <Grid2 item xs={12} sm={6} md={4} key={event.id}>
+            <EventCard item={event} />
+          </Grid2>
+        ))}
+      </Grid2>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ values, handleChange, setFieldValue, errors, touched }) => (
+              <Form>
+                <Grid2 className="my-3" container spacing={3}>
+                  <Grid2 size={{ xs: 12 }}>
+                    <Field
+                      name="image"
+                      label="Image URL"
+                      variant="outlined"
+                      fullWidth
+                      as={TextField}
+                      onChange={handleChange}
+                      value={values.image}
+                      error={touched.image && Boolean(errors.image)}
+                      helperText={touched.image && errors.image}
+                    />
+                  </Grid2>
+                  <Grid2 size={{ xs: 12 }}>
+                    <Field
+                      name="location"
+                      label="Location"
+                      variant="outlined"
+                      fullWidth
+                      as={TextField}
+                      onChange={handleChange}
+                      value={values.location}
+                      error={touched.location && Boolean(errors.location)}
+                      helperText={touched.location && errors.location}
+                    />
+                  </Grid2>
+                  <Grid2 size={{ xs: 12 }}>
+                    <Field
+                      name="name"
+                      label="Event Name"
+                      variant="outlined"
+                      fullWidth
+                      as={TextField}
+                      onChange={handleChange}
+                      value={values.name}
+                      error={touched.name && Boolean(errors.name)}
+                      helperText={touched.name && errors.name}
+                    />
+                  </Grid2>
+                  <Grid2 size={{ xs: 12 }}>
+                    <Field
+                      name="description"
+                      label="Event Description"
+                      variant="outlined"
+                      fullWidth
+                      as={TextField}
+                      multiline
+                      rows={4}
+                      onChange={handleChange}
+                      value={values.description}
+                      error={touched.description && Boolean(errors.description)}
+                      helperText={touched.description && errors.description}
+                    />
+                  </Grid2>
+                  <Grid2 size={{ xs: 12 }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DateTimePicker
+                        renderInput={(props) => <TextField {...props} />}
+                        label="Start Date and Time"
+                        value={values.startedAt}
+                        onChange={(newValue) =>
+                          setFieldValue("startedAt", newValue)
+                        }
+                        className="w-full"
+                      />
+                    </LocalizationProvider>
+                  </Grid2>
+                  <Grid2 size={{ xs: 12 }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DateTimePicker
+                        renderInput={(props) => <TextField {...props} />}
+                        label="End Date and Time"
+                        value={values.endsAt}
+                        onChange={(newValue) =>
+                          setFieldValue("endsAt", newValue)
+                        }
+                        className="w-full"
+                      />
+                    </LocalizationProvider>
+                  </Grid2>
+                </Grid2>
+                <Button className="my-2" variant="contained" type="submit">
+                  Create Event
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Modal>
     </div>
   );
 };
--- src/component/Profile/EventCard.jsx
+++ src/component/Profile/EventCard.jsx
@@ -2,37 +2,116 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   CardMedia,
+  Collapse,
   IconButton,
   Typography,
 } from "@mui/material";
 import React from "react";
+import { styled } from "@mui/material/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useLocation } from "react-router-dom";
 
-const EventCard = () => {
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
+const EventCard = ({ item }) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const location = useLocation();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const formatDate = (date) => {
+    return new Date(date).toLocaleString("it-IT", {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+
   return (
     <div>
-      <Card sx={{ width: 345 }}>
+      <Card
+        sx={{
+          maxWidth: 345,
+          minHeight: 450,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <CardHeader title={item.name} subheader={item.restaurant.name} />
         <CardMedia
-          sx={{ height: 345 }}
-          image="https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=600"
+          component="img"
+          height="194"
+          image={item.image}
+          alt="event"
+          sx={{
+            objectFit: "cover",
+          }}
         />
         <CardContent>
-          <Typography variant="h5">Burger's House</Typography>
-          <Typography variant="body2">Black Friday Event 50%</Typography>
-          <div className="py-2 space-y-2">
-            <p>{"Milano"}</p>
-            <p className="text-sm text-blue-500">22 Novembre 2024, 10.00</p>
-            <p className="text-sm text-red-500">23 Novembre 2024, 23.00</p>
-          </div>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {item.location}
+          </Typography>
+
+          <Typography variant="body2" sx={{ color: "#4354EB" }}>
+            {formatDate(item.startedAt)}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "#CD524E" }}>
+            {formatDate(item.endsAt)}
+          </Typography>
         </CardContent>
-        {true && (
-          <CardActions>
-            <IconButton>
+        <CardActions disableSpacing>
+          {location.pathname.includes("/admin/restaurants/event") && (
+            <IconButton aria-label="delete button" color="error">
               <DeleteIcon />
             </IconButton>
-          </CardActions>
-        )}
+          )}
+
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography sx={{ marginBottom: 2 }}>Description:</Typography>
+            <Typography sx={{ marginBottom: 2 }}>{item.description}</Typography>
+          </CardContent>
+        </Collapse>
       </Card>
     </div>
   );
